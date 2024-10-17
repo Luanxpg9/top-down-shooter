af2e20e14b111d8c0c4af44112e12ff12160fe46
michas-o-michas <michas-o-michas>
2024-10-17 02:04:21 -0300

    receba

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
-@onready var rack_sound: AudioStreamPlayer2D = $RackSound
-@onready var shot_sound: AudioStreamPlayer2D = $ShotSound
+@onready var rack_sound: AudioStreamPlayer2D = $Audios/RackSound
+@onready var shot_sound: AudioStreamPlayer2D = $Audios/ShotSound
+@onready var footstep_sound: AudioStreamPlayer2D = $Audios/FootstepSound
+@onready var point_light_2d_2_player: PointLight2D = $PointLight2D2Player
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+@onready var explosion: GPUParticles2D = $Explosion
+
 @onready var ProjectileScene: PackedScene = preload("res://projectile.tscn")
 const SPEED = 100
 @onready var spawn_bullet: Node2D = $SpawnBullet
 
 var can_shot:bool = false
 var shot_anim_finished = true
+var can_walk_sound = false
 
+func _ready() -> void:
+	animated_sprite_2d.play("idle")
 
 func _physics_process(delta: float) -> void:
 	_move()
@@ -18,22 +27,33 @@ func _input(event):
 	if Input.is_action_just_pressed("click_left"):
 		shoot()
 
-func _rotate()->void:
+func _move()->void:
 	var input_dir = Input.get_vector("a", "d", "w", "s")
+	if input_dir == Vector2.ZERO:
+		can_walk_sound = false
+		#animated_sprite_2d.play("idle")
+	elif not footstep_sound.playing:
+		if !animated_sprite_2d.is_playing():
+			animated_sprite_2d.play("move")
+		can_walk_sound = true
+	_sound_walking()
 	velocity = input_dir * SPEED
 
-func _move()->void:
+func _rotate()->void:
 	var mouse_position = get_global_mouse_position()
 	var direction = mouse_position - global_position
 	var angle = direction.angle()
 	rotation = angle
+	point_light_2d_2_player.rotation = -angle
+	
 	
 func shoot():
 	if not can_shot:
 		return
-	
+	explosion.emitting = true
 	shot_anim_finished = false
 	can_shot = false
+	animated_sprite_2d.play("shot")
 	shot_sound.play()
 	
 	var mouse_position = get_global_mouse_position()
@@ -69,21 +89,24 @@ func shoot():
 	get_parent().add_child(projectile_instance)
 	get_parent().add_child(projectile_instance_left)
 	get_parent().add_child(projectile_instance_right)
+	
 
 
-
-
-func _on_ray_cast_2d_child_entered_tree(node: Node) -> void:
-	print(1)
-	pass # Replace with function body.
-
+func _sound_walking():
+	if can_walk_sound and not footstep_sound.playing:
+		footstep_sound.play()
 
 func _on_timer_fire_rate_timeout() -> void:
+	explosion.emitting = false
 	if shot_anim_finished:
 		can_shot = true
 
 func _on_shot_sound_finished() -> void:
 	rack_sound.play()
+	animated_sprite_2d.play("reload")
 
 func _on_rack_sound_finished() -> void:
 	shot_anim_finished = true
+
+func _on_footstep_sound_finished() -> void:
+	can_walk_sound = true
--- projectile.gd
+++ projectile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var collision: CollisionShape2D = $Collision
 
-var speed = 4000 
+var speed = 4000
 var direction = Vector2.ZERO  
 
 
@@ -11,5 +11,11 @@ func _physics_process(delta: float):
 
 
 func _on_area_entered(area: Area2D) -> void:
-	pass
-	#queue_free()
+	if not area.is_in_group("projectile"):
+		queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == 'paredes':
+		queue_free()
+	
